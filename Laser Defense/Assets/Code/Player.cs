871b6f38d2cfe38778e8c40a7b4ccaa10787210e
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-07-20 21:22:24 -0400

    Player Movement

--- Laser Defense/Assets/Code/Player.cs
+++ Laser Defense/Assets/Code/Player.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    void Start()
-    {
-        
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    [SerializeField] float runSpeed = 5;
+    void Start(){
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
     }
 
  
-    void Update()
-    {
-        
+    void Update(){
+        Run();
+    }
+
+    void OnMove(InputValue value){
+        moveInput = value.Get<Vector2>();
+    }
+
+     void Run(){
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, moveInput.y * runSpeed);
+        myRigidbody.velocity = playerVelocity;
     }
 }
