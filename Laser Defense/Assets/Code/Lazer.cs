00e7c1e3d8250bb3052e0de5c50e9670ad7130d7
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-07-20 21:42:01 -0400

    Lazer Progress

---
+++ Laser Defense/Assets/Code/Lazer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour
+{
+    Rigidbody2D myRigidbody2d;
+    BoxCollider2D LazerBox;
+    Player player;
+    float playerDirection;
+    [SerializeField] float xArrow;
+    [SerializeField] float yArrow;
+    void Start(){
+        myRigidbody2d = GetComponentInChildren<Rigidbody2D>();
+        LazerBox = GetComponentInChildren<BoxCollider2D>();
+        player = FindObjectOfType<Player>();
+        playerDirection = player.transform.localScale.x * yArrow;
+    }
+
+    void Update(){
+        LazerMovement();
+    }
+
+    void LazerMovement(){
+        myRigidbody2d.velocity = new Vector2(xArrow, playerDirection);
+    }
+}
