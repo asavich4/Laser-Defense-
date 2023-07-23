2df6b4e15e2dd5b38beec77e8070d9c1c5f73671
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-07-23 14:39:33 -0400

    Borders

--- Laser Defense/Assets/Code/Player.cs
+++ Laser Defense/Assets/Code/Player.cs
@@ -7,6 +7,11 @@ public class Player : MonoBehaviour
 {
     Vector2 maxBounds;
     Vector2 minBounds;
+    [SerializeField] float padLeft;
+    [SerializeField] float padRight;
+    [SerializeField] float padBottom;
+    [SerializeField] float padTop;
+
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
@@ -38,9 +43,9 @@ public class Player : MonoBehaviour
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed * Time.deltaTime, moveInput.y * runSpeed * Time.deltaTime);
         myRigidbody.velocity = playerVelocity;
         Vector2 newPos = new Vector2();
-        newPos.x = Mathf.Clamp(transform.position.x + playerVelocity.x, minBounds.x, maxBounds.x);
-        newPos.y = Mathf.Clamp(transform.position.y + playerVelocity.y, minBounds.y, maxBounds.y);
-        transfom.position = newPos;
+        newPos.x = Mathf.Clamp(transform.position.x + playerVelocity.x, minBounds.x + padLeft, maxBounds.x - padRight);
+        newPos.y = Mathf.Clamp(transform.position.y + playerVelocity.y, minBounds.y + padBottom, maxBounds.y - padTop);
+        transform.position = newPos;
 
     }
 
