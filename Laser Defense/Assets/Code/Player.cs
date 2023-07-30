27f6ac20a37e976dbdecf707c075d12e1697e4a9
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-07-30 14:26:16 -0400

    Fire Delay

--- Laser Defense/Assets/Code/Player.cs
+++ Laser Defense/Assets/Code/Player.cs
@@ -19,47 +19,55 @@ public class Player : MonoBehaviour
     [SerializeField] float runSpeed = 5;
 
     [SerializeField] GameObject Lazer;
-    [SerializeField]Transform Gun;
-    
+    [SerializeField] Transform Gun;
+
     [SerializeField] AudioClip shootSound;
+    
+    [SerializeField] float fireCooldown = 0.5f;
+    private bool canFire = true;
+
     void Start(){
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         Bounds();
     }
 
- 
     void Update(){
         Run();
+        if (canFire && Input.GetButton("Fire1")){
+            StartCoroutine(FireCooldown());
+        }
     }
 
-     void OnFire(InputValue value){
+    IEnumerator FireCooldown(){
+        canFire = false;
         Instantiate(Lazer, Gun.position, transform.rotation);
         AudioSource.PlayClipAtPoint(shootSound, Camera.main.transform.position);
+        yield return new WaitForSeconds(fireCooldown);
+        canFire = true;
     }
 
     void OnMove(InputValue value){
         moveInput = value.Get<Vector2>();
     }
 
-    void  OnCollisionEnter2D(Collision2D collision) {
+    void OnCollisionEnter2D(Collision2D collision){
         Destroy(gameObject);
         Destroy(collision.gameObject);
     }
 
-     void Run(){
+    void Run(){
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed * Time.deltaTime, moveInput.y * runSpeed * Time.deltaTime);
         myRigidbody.velocity = playerVelocity;
         Vector2 newPos = new Vector2();
         newPos.x = Mathf.Clamp(transform.position.x + playerVelocity.x, minBounds.x + padLeft, maxBounds.x - padRight);
         newPos.y = Mathf.Clamp(transform.position.y + playerVelocity.y, minBounds.y + padBottom, maxBounds.y - padTop);
         transform.position = newPos;
-
     }
 
     void Bounds(){
         Camera mainCamera = Camera.main;
-        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
-        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 }
